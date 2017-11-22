25c98a5b7aa6133dee4fbee81126b94040fed9b8
Dan Frank <df@stripe.com>
2017-11-21 18:31:33 -0800

    Add routing and echo service, get runnable via IJ

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,14 +1,37 @@
-import com.twitter.finagle.{Http, Service}
-import com.twitter.finagle.http
+import com.twitter.finagle.http.{Request, Response, Status}
+import com.twitter.finagle.http.codec.HttpCodec
+import com.twitter.finagle.http.path.{ParamMatcher, Root}
+import com.twitter.finagle.http.service.RoutingService
+import com.twitter.finagle.{Http, Service, http}
 import com.twitter.util.{Await, Future}
+import com.twitter.finagle.http.path._
 
 object Server extends App {
-  val service = new Service[http.Request, http.Response] {
-    def apply(req: http.Request): Future[http.Response] =
+  val service = new Service[Request, Response] {
+    def apply(req: Request): Future[Response] = {
+      val x = Response(req.version, Status.Ok)
+      x.setContentString("yo")
       Future.value(
-        http.Response(req.version, http.Status.Ok)
+        x
       )
+    }
   }
-  val server = Http.serve(":8080", service)
+
+  def echoService(message: String) = new Service[Request, Response] {
+    def apply(req: Request): Future[Response] = {
+      val rep = Response(req.version, Status.Ok)
+      rep.setContentString(message)
+
+      Future(rep)
+    }
+  }
+
+  object EchoParamMatch extends ParamMatcher("reqText")
+
+  val routingService = RoutingService.byPathObject[Request] {
+    case Root => service
+    case Root /  "echo" / message => echoService(message)
+  }
+  val server = Http.serve(":8080", routingService)
   Await.ready(server)
 }
