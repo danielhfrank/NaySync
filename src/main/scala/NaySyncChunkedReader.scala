48acefdaa6bff916d21e821b73a3f290194c25d1
Dan Frank <df@stripe.com>
2017-11-27 18:14:10 -0800

    Revert "thrash around with alt impl"
    This reverts commit ee7fe4d3bb6a1b49d8e36419478878749f550d3e.
    because it was just curl being silly

--- src/main/scala/NaySyncChunkedReader.scala
+++ src/main/scala/NaySyncChunkedReader.scala
@@ -1,34 +1,6 @@
-import com.twitter.concurrent.{AsyncMutex, AsyncStream}
+import com.twitter.concurrent.AsyncStream
 import com.twitter.io.{Buf, Reader}
 import com.twitter.util.{Future, Promise}
-
-class NaySyncChunkedReader(requestId: String, resultPromise: Future[Buf]) extends Reader {
-  private val bytes = requestId.getBytes("utf-8")
-  val reqIdBuf = Buf.ByteArray.Owned(bytes)
-  val reqIdReader: Reader = Reader.fromBuf(reqIdBuf)
-  val futureContentReader: Future[Reader] = resultPromise.map { result => Reader.fromBuf(result) }
-
-  private[this] val mutex = new AsyncMutex()
-  @volatile private[this] var idHasBeenRead = false
-
-
-  override def read(n: Int): Future[Option[Buf]] = {
-    mutex.acquireAndRun{
-      if(!idHasBeenRead){
-        println(s"Serving the read for the request id with n=$n out of ${bytes.size}")
-        idHasBeenRead = true // TODO gnar around if not entire thing is read??
-        reqIdReader.read(n)
-      } else {
-        futureContentReader.flatMap(_.read(n))
-      }
-    }
-  }
-
-  override def discard(): Unit = {
-    reqIdReader.discard()
-    futureContentReader.foreach(_.discard())
-  }
-}
 
 object NaySyncChunkedReader {
 
